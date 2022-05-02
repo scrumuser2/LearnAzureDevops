aea09b4fb3d7203280d89c9b8112624507f1c7b4
scrumuser2 <104751837+scrumuser2@users.noreply.github.com>
2022-05-02 16:09:39 +0530

    Add files via upload

---
+++ LearnDesignPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------Design Patterns------------------");
+            Console.WriteLine("creational pattern Singleton");
+            Console.ReadKey();
+        }
+    }
+}
